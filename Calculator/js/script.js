6890822eb7881f0ce7cde49f92b9089a8e6c103a
ojusvi <60205487+ojusvi@users.noreply.github.com>
2023-10-19 21:57:41 +0530

    Update script.js

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,16 +1,14 @@
 let string = "";
 
-let display = document.getElementById('calc-display'); // Get the input field by its id
+let display = document.getElementById('calc-display');
 
 let inputs = document.querySelectorAll('input[type="button"]');
 
-// Function to update the display and the string
 function updateDisplay(value) {
     string = value;
     display.value = string;
 }
 
-// Add an event listener for keydown on the document
 document.addEventListener('keydown', (event) => {
     if (event.key >= '0' && event.key <= '9') {
         string = string + event.key;
@@ -22,15 +20,12 @@ document.addEventListener('keydown', (event) => {
         string = string + event.key;
         updateDisplay(string);
     } else if (event.key === 'Enter') {
-        // Evaluate the expression when the 'Enter' key is pressed
         string = eval(string);
         updateDisplay(string);
     } else if (event.key === 'Escape') {
-        // Clear the display when the 'Escape' key is pressed
         string = '';
         updateDisplay(string);
     } else if (event.key === 'Backspace') {
-        // Delete the last character when the 'Backspace' key is pressed
         if (string.length > 0) {
             string = string.slice(0, -1);
             updateDisplay(string);
@@ -38,7 +33,6 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
-// Add click event listeners for the buttons
 Array.from(inputs).forEach((input) => {
     input.addEventListener('click', (event) => {
         if (event.target.value == '=') {
